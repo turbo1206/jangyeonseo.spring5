c2bf3c55550f3e10f6826e4225bccd5fd8a808cf
User <User@DESKTOP-KONC19A>
2021-05-26 16:40:41 +0900

    7교시 커밋

--- src/test/java/kr/or/test/HelloWorld.java
+++ src/test/java/kr/or/test/HelloWorld.java
@@ -3,7 +3,7 @@ package kr.or.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// main 자바프로그램의 진입 메서드(자동실행)입니다.
+		// classpath > exclude test code unchecked 해야지만, 오류안남.
 		System.out.println("헬로 자바!");
 	}
 

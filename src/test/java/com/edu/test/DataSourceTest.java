8d77211fd08ccaaf345ac04c6464dbe4c2b7f491
User <User@DESKTOP-KONC19A>
2021-06-07 17:40:36 +0900

    8교시

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -19,7 +19,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 /**
  * 이 클래스는 오라클과 연동해서 CRUD를 테스트하는 클래스 입니다.
  * 과장(이사,팀장) JUnit CRUD까지 만들어서 일반사원에게 공개 + 회원관리
- * @author 김일국
+ * @author 장연서
  *
  */
 //RunWith인터페이스 현재클래스가 Junit실행클래스라고 명시
@@ -35,11 +35,21 @@ public class DataSourceTest {
 	DataSource dataSource;//Inject로 객체를 만들면 메모리 관리를 스프링이 대신해 줌.
 	//Inject 자바8부터 지원, 그럼, 이전 자바7에서 @Autowired 로 객체를 만들었슴
 	
+	//스프링 코딩 시작 순서
+	//M-V-C 사이에 데이터를 입출력하는 임시저장 공간(VO클래스를-멤버변수+Get/Set메서드) 생성
+	//보통 ValueObject클래스는 DB테이블과 1:1로 매칭이 됩니다.
+	//그래서, 1. MemberVO.Java VO클래스를 생성.(필수)
+	//2. DB(마이바티스)쿼리를 만듭니다.(VO사용됨) - 20210608 시작
+	@Test
+	public void selectMember() throws Exception {
+		//회원관리 테이블에서 더미로 입력한 100개의 레코드를 출력 메서드 테스트->회원관리목록이 출력
+		//스프링 코딩 순서
+	}
 	@Test
 	public void oldQueryTest() throws Exception {
 		//스프링빈을 사용하지 않을때 예전 방식: 코딩테스트에서는 스프링설정을 안쓰고, 직접 DB 아이디/암호 입력
 		Connection connection = null;
-		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE","apmsetup");
+		connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE","XE2","apmsetup");
 		logger.debug("데이터베이스 직접 접속이 성공 하였습니다. DB종류는 "+ connection.getMetaData().getDatabaseProductName());
 		//직접쿼리를 날립니다. 날리기전 쿼리문장 객체생성statement
 		Statement stmt = connection.createStatement();

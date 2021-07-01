de93a39b70584105eff3a0310ee787771066d841
turbo <turbo@110.15.182.114>
2021-07-01 16:54:28 +0900

    커밋

--- src/main/java/com/edu/controller/HomeController.java
+++ src/main/java/com/edu/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.edu.controller;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //외부 라이브러리(모듈) 사용 = import
@@ -7,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.edu.service.IF_MemberService;
 
 /**
  * 이 클래스는 MVC웹프로젝트를 최초로 생성시 자동으로 생성되는 클래스
@@ -30,6 +36,20 @@ public class HomeController {
 	 */
 	//이제부터 일반적인 개발방시 VO->쿼리->DAO->Service(관리자단에서 여기까지끝)
 	//관리자단에서 작성한 Service 사용자단에서 그대로 이용, 컨트롤러부터 분리해작업->jsp
+	@Inject
+	private IF_MemberService memberService;
+	
+	//마이페이지 폼호출 GET방식, 회원수정폼이기때문에 model담아서 변수값을 전송이 필요
+	@RequestMapping(value="/member/mypage_form", method=RequestMethod.GET)
+	public String mypage_form(HttpServletRequest request, Model model) throws Exception {
+		//로그인 한 사용자 세션을 session_userid로 memberService의 readMember를 호출하면됨.
+		//jsp에서 발생된 세션을 가져오려고 하기 때문에 HttpServletRequest객체가 사용됩니다.
+		HttpSession session = request.getSession();//싱클톤 객체
+		String user_id = (String) session.getAttribute("session_userid");
+		//memberService에서 1개의 레코드를 가져옵니다. model담아서 jsp로 보냅니다.
+		model.addAttribute("memberVO", memberService.readMember(user_id));
+		return "home/member/mypage";//.jsp생략
+	}
 	//사용자단 로그인 폼호출 GET, 로그인POST처리는 컨트롤러에서 하지않고 스프링시큐리티로 처리
 	@RequestMapping(value="/login_form", method=RequestMethod.GET)
 	public String login_form() throws Exception {

88783b94640df0f3f0eb89fa94de861a3f12f5b1
turbo <turbo@114.200.149.91>
2021-07-09 17:02:23 +0900

    커밋

--- src/main/java/com/edu/service/MemberServiceImpl.java
+++ src/main/java/com/edu/service/MemberServiceImpl.java
@@ -32,7 +32,7 @@ public class MemberServiceImpl implements IF_MemberService {
 		//한국시간 월-금 8-23시까지(미국시간 0-14) 헤로쿠앱에 20분간격으로 접근
 		//헤로쿠 컨테이너가 러닝할 수 있는 무료 시간 1달 700시간 정보 됩니다. 시간이 넘으면 먹통(1달내)
 		//주말에 이력서보시는 분이 없기때문에
-		String urlStr = "https://kimilguk-spring5.herokuapp.com/";
+		String urlStr = "https://jangyeonseo-spring5.herokuapp.com/";
 		URL url = new URL(urlStr);
 		HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();//접속객체만들기
 		urlConnection.setUseCaches(false);//접속시 캐시사용없이 무조건 새로고침하겠다고 명시, 컨테이너가 계속 떠있습니다.(세션도 유지됨)
